bee6927fe5ef088c6942e53d3917c074ea4f1d33
roycechip <215168035+roycechip@users.noreply.github.com>
2026-03-14 22:33:08 +0800

    Made canvas wrap around the image
    Adjusted so that the canvas fits and keeps the image aspect ratio

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 const uploadButton = document.querySelector('#uploadButton');
 const fileInput = document.querySelector('#fileInput');
 const canvas = document.querySelector('#canvas');
-let canvaswidth = canvas.width;
-let canvasheight = canvas.height;
 
 let img = new Image();
 
 img.addEventListener('load', (e) => {
     let ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0, canvaswidth, canvasheight);
+
+    let maxWidth = 500;
+    let maxHeight = 500;
+    let widthScale = maxWidth/img.width;
+    let heightScale = maxHeight/img.height;
+    let scale = Math.min(widthScale, heightScale);
+    if(img.width <= maxWidth && img.height <= maxHeight){
+        scale = 1;
+        console.log('smol');
+    }
+    canvas.width = img.width*scale;
+    canvas.height = img.height*scale;
+    ctx.drawImage(img, 0, 0, img.width*scale, img.height*scale);
 })
 
 uploadButton.addEventListener('click', () => fileInput.click());
 
 fileInput.addEventListener('change', () => {
-    if(fileInput.files[0]['type'].split('/')[0]!=='image'){
-        console.log('ERROR. Not an image file');
+    let inputResult = fileInput.files[0]['type'].split('/');
+    let supportedImageTypes = ['jpeg', 'jpg', 'png'];
+    if(inputResult[0]!=='image' || !supportedImageTypes.includes(inputResult[1])){
+        console.log('ERROR. Not an image file or not supported image type (supports .jpeg, .jpg and .png)');
         return;
     }
-    //console.log("that's an image");
 
     const reader = new FileReader();
-    reader.addEventListener('load', (e) => /*console.log(e.target.result)*/ img.src = e.target.result);
+    reader.addEventListener('load', (e) => img.src = e.target.result);
     reader.readAsDataURL(fileInput.files[0]);
 });
