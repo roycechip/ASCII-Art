8a1760570770934cc395891c39397d28c85acae5
roycechip <215168035+roycechip@users.noreply.github.com>
2026-03-14 21:55:55 +0800

    Create basic image upload system
    Created a system where user can upload image and display on a canvas

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 const uploadButton = document.querySelector('#uploadButton');
 const fileInput = document.querySelector('#fileInput');
+const canvas = document.querySelector('#canvas');
+let canvaswidth = canvas.width;
+let canvasheight = canvas.height;
+
+let img = new Image();
+
+img.addEventListener('load', (e) => {
+    let ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, canvaswidth, canvasheight);
+})
 
 uploadButton.addEventListener('click', () => fileInput.click());
+
 fileInput.addEventListener('change', () => {
     if(fileInput.files[0]['type'].split('/')[0]!=='image'){
         console.log('ERROR. Not an image file');
         return;
     }
-    console.log("that's an image");
+    //console.log("that's an image");
 
+    const reader = new FileReader();
+    reader.addEventListener('load', (e) => /*console.log(e.target.result)*/ img.src = e.target.result);
+    reader.readAsDataURL(fileInput.files[0]);
 });
