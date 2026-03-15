b9954a5991bc939dc5f8d62aaef492207d2f51d5
roycechip <215168035+roycechip@users.noreply.github.com>
2026-03-15 17:46:42 +0800

    Create reference canvas
    Pixel data converting to ASCII will be extracted from reference canvas

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 const uploadButton = document.querySelector('#uploadButton');
 const fileInput = document.querySelector('#fileInput');
 const canvas = document.querySelector('#canvas');
+const canvas1 = document.querySelector('#canvas1')
 
 let img = new Image();
 
 img.addEventListener('load', (e) => {
     let ctx = canvas.getContext('2d');
+    let ctx1 = canvas1.getContext('2d');
 
-    let maxWidth = 500;
-    let maxHeight = 500;
-    let widthScale = maxWidth/img.width;
-    let heightScale = maxHeight/img.height;
-    let scale = Math.min(widthScale, heightScale);
-    if(img.width <= maxWidth && img.height <= maxHeight){
-        scale = 1;
+    let maxDisplayWidth = 500;
+    let maxDisplayHeight = 500;
+    let maxReferenceWidth = 300;
+    let maxReferenceHeight = 300;
+
+    let displayWidthScale = maxDisplayWidth/img.width;
+    let displayHeightScale = maxDisplayHeight/img.height;
+    let referenceWidthScale = maxReferenceWidth/img.width;
+    let referenceHeightScale = maxReferenceHeight/img.height;
+
+    let displayScale = Math.min(displayWidthScale, displayHeightScale);
+    let referenceScale = Math.min(referenceWidthScale, referenceHeightScale);
+    if(img.width <= maxDisplayWidth && img.height <= maxDisplayHeight){
+        displayScale = 1;
     }
-    canvas.width = img.width*scale;
-    canvas.height = img.height*scale;
-    ctx.drawImage(img, 0, 0, img.width*scale, img.height*scale);
+    if(img.width <= maxReferenceWidth && img.height <= maxReferenceHeight){
+        referenceScale = 1;
+    }
+
+    canvas.width = img.width*displayScale;
+    canvas.height = img.height*displayScale;
+
+    canvas1.width = img.width*referenceScale;
+    canvas1.height = img.height*referenceScale;
+    ctx.drawImage(img, 0, 0, img.width*displayScale, img.height*displayScale);
+    ctx1.drawImage(img, 0, 0, img.width*referenceScale, img.height*referenceScale);
 })
 
 uploadButton.addEventListener('click', () => fileInput.click());
